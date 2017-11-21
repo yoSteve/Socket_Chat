dc8d2a6375b7532b30e0b00c86c31a4a06e0cf4a
yoSteve <steve.yorke@gmail.com>
2017-11-21 13:38:29 -0800

    Created client-side from to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -2,11 +2,20 @@ const socket = io();
 
 socket.on('connect', () => {
     console.log('Connected to socket server!');
-    socket.emit('message', {
-        text: "OMG I'm emitting!"
-    })
 });
 
 socket.on('message', (message) => {
     console.log('New message:', message.text);
+});
+
+// Handle new message form submission
+const $form = $('#message-form');
+
+$form.on('submit', (event) => {
+    event.preventDefault();
+    $message = $form.find('input[name=message]');    
+    socket.emit('message', {
+        text: $message.val()
+    });
+    $message.val('');
 });
