a55719421f24ffcfc7e30e8fe5eda761d1e71650
yoSteve <steve.yorke@gmail.com>
2017-11-21 11:55:10 -0800

    Added socket.io on client and server

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
+// serve client-side files from ./public
 app.use(express.static(__dirname + '/public'));
+
+// socket.io listening for connection event
+io.on('connection', () => {
+    console.log('User connected via socket.io!');
+});
 
 http.listen(PORT, () => {
     console.log('Server listening on PORT ' + PORT);
