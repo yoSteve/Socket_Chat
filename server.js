5e52b373b64d310c1257af4e2a8647c148ff74e5
yoSteve <steve.yorke@gmail.com>
2017-11-21 11:25:41 -0800

    Basic server is listening and serving /public/index.html

---
+++ server.js
@@ -0,0 +1,10 @@
+const PORT = process.env.PORT || 3000;
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+
+app.use(express.static(__dirname + '/public'));
+
+http.listen(PORT, () => {
+    console.log('Server listening on PORT ' + PORT);
+});
